e1caefd1a1757633f336cb80db0780e9d64e828a
rahmazaki20 <rahmaramahmoud@gmail.com>
2025-07-06 23:17:54 +0200

    formatting document

--- script.js
+++ script.js
@@ -1,27 +1,26 @@
+const registerBtn = document.querySelector(".register-button");
+const popup = document.getElementById("contact-popup");
 
-  const registerBtn = document.querySelector('.register-button');
-  const popup = document.getElementById('contact-popup');
+registerBtn.addEventListener("click", function (e) {
+  e.preventDefault(); // Prevent link default
+  popup.style.display = "flex";
+  document.body.style.overflow = "hidden"; // prevent background scroll
+});
 
-  registerBtn.addEventListener('click', function (e) {
-    e.preventDefault(); // Prevent link default
-    popup.style.display = 'flex';
-    document.body.style.overflow = 'hidden'; // prevent background scroll
-  });
+function closePopup() {
+  popup.style.display = "none";
+  document.body.style.overflow = ""; // re-enable scroll
+}
 
-  function closePopup() {
-    popup.style.display = 'none';
-    document.body.style.overflow = ''; // re-enable scroll
+document.addEventListener("DOMContentLoaded", function () {
+  const playIcon = document.querySelector(".play-icon");
+
+  if (playIcon) {
+    playIcon.addEventListener("click", () => {
+      playIcon.style.opacity = "0";
+      setTimeout(() => {
+        playIcon.style.display = "none";
+      }, 300); // match the CSS transition
+    });
   }
-
-  document.addEventListener("DOMContentLoaded", function () {
-    const playIcon = document.querySelector(".play-icon");
-
-    if (playIcon) {
-      playIcon.addEventListener("click", () => {
-        playIcon.style.opacity = "0";
-        setTimeout(() => {
-          playIcon.style.display = "none";
-        }, 300); // match the CSS transition
-      });
-    }
-  });
+});
